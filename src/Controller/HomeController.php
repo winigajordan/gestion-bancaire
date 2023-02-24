da60a4be68cc2de0589add58165086fd1153b71b
jord <winigajordan@gmail.com>
2023-02-24 21:17:52 +0000

    Integration Connexion

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Webmozart\Assert\Tests\StaticAnalysis\null;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        if (!$this->getUser()){
+            return $this->redirectToRoute('app_login');
+        }
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Caissier;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+    public function load(ObjectManager $manager): void
+    {
+       $caissier = (new Caissier())
+            ->setNom('Nom Caissier 1')
+            ->setPrenom('Prenom Caissier 1')
+           ->setIdentifiant(uniqid('ca-'))
+               ->setEmail("caissier@lightbank.com");
+       $password = $this->hasher->hashPassword($caissier, '1234');
+       $caissier->setPassword($password);
+       $manager->persist($caissier);
+       $manager->flush();
+    }
+}
